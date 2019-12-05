304b0d3bd0d8510f8a63317c5352047004950346
Anatoliy <AnatoliyLastochkin@gmail.com>
2019-12-05 12:37:31 +0100

    openning menu was implemented

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+var pageHeader = document.querySelector('.page-header');
+var headerToggle = pageHeader.querySelector('.page-header__toggle');
+var mainNav = pageHeader.querySelector('.main-nav');
+
+mainNav.classList.remove('main-nav--nojs');
+
+headerToggle.addEventListener('click', function (evt) {
+  mainNav.classList.toggle('main-nav--closed');
+  pageHeader.classList.toggle('page-header--blue');
+});
