740ae2891a479e8be49d73b71ed9ab6fa3514c21
Anatoliy <AnatoliyLastochkin@gmail.com>
2019-12-09 20:41:34 +0100

    mobile version was implemented

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var pageHeader = document.querySelector('.page-header');
 var headerToggle = pageHeader.querySelector('.page-header__toggle');
 var mainNav = pageHeader.querySelector('.main-nav');
 
+pageHeader.classList.remove('page-header--nojs');
 mainNav.classList.remove('main-nav--nojs');
 
 headerToggle.addEventListener('click', function (evt) {
