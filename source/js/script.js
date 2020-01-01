5ed61264498092d419038478a615dfef0664e0c6
Anatoliy <AnatoliyLastochkin@gmail.com>
2020-01-01 15:17:51 +0100

    menu for 3 version was created

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,20 @@
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = pageHeader.querySelector('.page-header__toggle');
-var mainNav = pageHeader.querySelector('.main-nav');
-var mainNavSiteList = mainNav.querySelector('.main-nav__site-list');
-var mainNavContact = mainNav.querySelector('.main-nav__contact');
-var mainNavSocial = mainNav.querySelector('.main-nav__social');
+var pageHeaderWrapper = document.querySelector('.page-header__wrapper');
+var headerToggle = pageHeaderWrapper.querySelector('.page-header__toggle');
+var mainNav = pageHeaderWrapper.querySelector('.main-nav');
+// var mainNavSiteList = mainNav.querySelector('.main-nav__site-list');
+// var mainNavContact = mainNav.querySelector('.main-nav__contact');
+// var mainNavSocial = mainNav.querySelector('.main-nav__social');
 
-pageHeader.classList.remove('page-header--nojs');
-pageHeader.classList.remove('page-header--white');
+pageHeaderWrapper.classList.remove('page-header__wrapper--nojs');
 mainNav.classList.remove('main-nav--nojs');
-mainNavSiteList.classList.remove('main-nav__site-list--open');
-mainNavContact.classList.remove('main-nav__contact--open');
-mainNavSocial.classList.remove('main-nav__social--open');
+// mainNavSiteList.classList.remove('main-nav__site-list--open');
+// mainNavContact.classList.remove('main-nav__contact--open');
+// mainNavSocial.classList.remove('main-nav__social--open');
 
 headerToggle.addEventListener('click', function (evt) {
-  mainNav.classList.toggle('main-nav--closed');
-  pageHeader.classList.toggle('page-header--white');
-  mainNavSiteList.classList.toggle('main-nav__site-list--open');
-  mainNavContact.classList.toggle('main-nav__contact--open');
-  mainNavSocial.classList.toggle('main-nav__social--open');
+  mainNav.classList.toggle('main-nav--opened');
+  pageHeaderWrapper.classList.toggle('page-header__wrapper--opened');
+  // mainNavSiteList.classList.toggle('main-nav__site-list--open');
+  // mainNavContact.classList.toggle('main-nav__contact--open');
+  // mainNavSocial.classList.toggle('main-nav__social--open');
 });
